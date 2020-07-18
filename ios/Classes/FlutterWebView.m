3125f215e5ce67d63a27ce087449970c96b85dbc
淡然 <smallsevenk@vip.qq.com>
2020-07-18 22:08:01 +0800

    Update FlutterWebView.m

--- ios/Classes/FlutterWebView.m
+++ ios/Classes/FlutterWebView.m
@@ -5,8 +5,7 @@
 #import "FlutterWebView.h"
 #import "FLTWKNavigationDelegate.h"
 #import "FLTWKProgressionDelegate.h"
-#import "JavaScriptChannelHandler.h"
-#import "UIColor+Kit.h"
+#import "JavaScriptChannelHandler.h" 
 
 #define RGBHex(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 #define RGBHexAlpha(rgbValue,a) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:(a)]
